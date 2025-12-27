bb920d3298117530e5d92a5a1701c63cdbb59310
Harsh N Patel <harshnpatel9@gmail.com>
2025-12-27 17:27:46 +0530

    fix: Update checksums for v1.0.1

--- Formula/stash.rb
+++ Formula/stash.rb
@@ -7,10 +7,10 @@ class Stash < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/harshpatel5940/stash/releases/download/v1.0.1/stash_1.0.1_darwin_arm64.tar.gz"
-      sha256 "REPLACE_WITH_ARM64_CHECKSUM"
+      sha256 "57fd2beccc976301fe4be7f6e4d099c390a25112b86c396cc6ef9331325310c2"
     else
       url "https://github.com/harshpatel5940/stash/releases/download/v1.0.1/stash_1.0.1_darwin_amd64.tar.gz"
-      sha256 "REPLACE_WITH_AMD64_CHECKSUM"
+      sha256 "bcc280a2d32fbd60a6057dbc323824444ec0a20e2a8352903c6c7757e56b67a3"
     end
   end
 
